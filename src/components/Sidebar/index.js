88577fd7dc04040fd68fdff899e1b5eb045d1db4
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2022-08-23 12:39:25 +0530

    complete SIde bar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,10 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faBehance, faGithub, faInstagram, faLinkedin, faTelegram, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
 
@@ -9,8 +12,59 @@ const Sidebar = () => (
         <Link className='logo' to='/'>
             <img src={LogoS} alt="logo" />
             <img className="sub-logo" src={LogoSubtitle} alt="logo" alt="Nipuna" />
-
         </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+
+        <ul>
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/madushan-nipuna-b73975173">
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://github.com/Madushan97">
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://www.youtube.com/channel/UCFYRi9r1lpTIeH3Vpq-b0kw">
+                    <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+                </a>
+            </li>
+
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://www.instagram.com/19nipuna97/">
+                    <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+                </a>
+            </li>
+
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://t.me/M_dushan">
+                    <FontAwesomeIcon icon={faTelegram} color="#4d4d4e" />
+                </a>
+            </li>
+
+            <li>
+                <a target="_blank" rel="noreferrer" href="https://www.behance.net/madushannipuna">
+                    <FontAwesomeIcon icon={faBehance} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
+
     </div>
 )
 
