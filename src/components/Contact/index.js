52e712c07de0da9684517489d2aa90f1ded9e54c
Madushan Nipuna Shantharathna <66712958+Madushan97@users.noreply.github.com>
2022-08-28 21:19:33 +0530

    contact form with emailJs, my location

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,14 @@
 import Loader from 'react-loaders'
 import './index.scss'
 import AnimattedLetters from '../AnimatedLetters'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const Contact = () => {
 
     const [letterClass, setLetterClass] = useState('text-animate')
-
+    const refForm = useRef()
 
     useEffect(() => {
 
@@ -14,6 +16,28 @@ const Contact = () => {
             setLetterClass('text-animate-hover')
         }, 3000)
     }, [])
+
+    const sendEmail = (e) => {
+
+        e.preventDefault()
+
+        emailjs
+            .sendForm(
+                'service_zjy5nm5',
+                'template_cqq6y9k',
+                refForm.current,
+                'user_hYlNlgi1lKYDwtaqJXz7I'
+            )
+            .then(
+                () => {
+                    alert('Message successfully sent :)')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send the message, Please try again')
+                }
+            )
+    }
 
     return (
 
@@ -34,7 +58,7 @@ const Contact = () => {
                     </p>
 
                     <div className='contact-form'>
-                        <form>
+                        <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className='half'>
                                     <input type='text' name='name' placeholder='Name' required />
@@ -58,6 +82,25 @@ const Contact = () => {
                             </ul>
                         </form>
                     </div>
+                </div>
+
+                {/* Map */}
+                <div className="info-map">
+                    Madushan Nipuna
+                    <br />
+                    Sri Lanka,
+                    <br />
+                    274/1, "Sirisevana", Malvatta Road, <br /> Higgashen, Kuruwita, 70500 <br />
+                    <span>madushannipuna1997@gmail.com</span>
+                </div>
+
+                <div className='map-wrap'>
+                    <MapContainer center={[44.96366, 19.61045]}>
+                        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                        <Marker position={[44.96366, 19.61045]}>
+                            <Popup>Madushan lives here, come over for a cup of coffee 	&#9749;</Popup>
+                        </Marker>
+                    </MapContainer>
                 </div>
             </div>
             <Loader type='pacman' />
